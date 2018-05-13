b1cb3307edb4c5f0645a45325c50da0def52a810
David DANG <david.ddkino@gmail.com>
2018-05-13 14:40:35 +0200

    mongoose

--- _test_/routes/index.js
+++
@@ -1,6 +0,0 @@
-export { default as campaignRouter } from './campaign';
-export { default as dokkuRouter } from './dokku';
-export { default as exportRouter } from './export';
-export { default as graphqlRouter } from './graphql';
-export { default as loginRouter } from './login';
-export { default as pdfRouter } from './pdf';
